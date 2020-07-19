f0d0f23c6d672ad29047d9e10df6ca776dd585ed
Ridha Rahmi <ridha.rahmi13@gmail.com>
2020-07-19 22:58:16 +0100

    Updates

---
+++ precache-manifest.a7253a890b82d6f6fee7ff8063d9608d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566efe17ef1d424b2770908f81bc6607",
+    "url": "/bootstrap-react-js/index.html"
+  },
+  {
+    "revision": "855e850453d465b60c84",
+    "url": "/bootstrap-react-js/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7bee83664191e91c76c4",
+    "url": "/bootstrap-react-js/static/css/main.0d14e611.chunk.css"
+  },
+  {
+    "revision": "855e850453d465b60c84",
+    "url": "/bootstrap-react-js/static/js/2.4a6b3834.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bootstrap-react-js/static/js/2.4a6b3834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bee83664191e91c76c4",
+    "url": "/bootstrap-react-js/static/js/main.0bef69e2.chunk.js"
+  },
+  {
+    "revision": "1ebe6ad186ab9f3e08f1",
+    "url": "/bootstrap-react-js/static/js/runtime-main.598b08c5.js"
+  },
+  {
+    "revision": "1e9e93d863b7811934889f9aac89c7de",
+    "url": "/bootstrap-react-js/static/media/bootstrap.1e9e93d8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bootstrap-react-js/static/media/logo.5d5d9eef.svg"
+  }
+]);
